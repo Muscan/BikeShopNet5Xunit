cae958291dbb50ffacde318576c4557361321093
Mihai <kubikweg@gmail.com>
2021-10-21 00:03:45 +0300

    Initial commit for a .net5 project with xunit

---
+++ BikeShopDotNet5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BikeShopDotNet5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
